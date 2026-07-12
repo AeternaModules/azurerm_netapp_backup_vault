1ffcbb35e56c942d7df7869fbb84addf4af35f53
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "netapp_backup_vaults" {
-  description = "All netapp_backup_vault resources"
-  value       = azurerm_netapp_backup_vault.netapp_backup_vaults
-}
 output "netapp_backup_vaults_account_name" {
-  description = "List of account_name values across all netapp_backup_vaults"
-  value       = [for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : v.account_name]
+  description = "Map of account_name values across all netapp_backup_vaults, keyed the same as var.netapp_backup_vaults"
+  value       = { for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : k => v.account_name }
 }
 output "netapp_backup_vaults_location" {
-  description = "List of location values across all netapp_backup_vaults"
-  value       = [for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : v.location]
+  description = "Map of location values across all netapp_backup_vaults, keyed the same as var.netapp_backup_vaults"
+  value       = { for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : k => v.location }
 }
 output "netapp_backup_vaults_name" {
-  description = "List of name values across all netapp_backup_vaults"
-  value       = [for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : v.name]
+  description = "Map of name values across all netapp_backup_vaults, keyed the same as var.netapp_backup_vaults"
+  value       = { for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : k => v.name }
 }
 output "netapp_backup_vaults_resource_group_name" {
-  description = "List of resource_group_name values across all netapp_backup_vaults"
-  value       = [for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : v.resource_group_name]
+  description = "Map of resource_group_name values across all netapp_backup_vaults, keyed the same as var.netapp_backup_vaults"
+  value       = { for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : k => v.resource_group_name }
 }
 output "netapp_backup_vaults_tags" {
-  description = "List of tags values across all netapp_backup_vaults"
-  value       = [for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : v.tags]
+  description = "Map of tags values across all netapp_backup_vaults, keyed the same as var.netapp_backup_vaults"
+  value       = { for k, v in azurerm_netapp_backup_vault.netapp_backup_vaults : k => v.tags }
 }
 
